3a95f6ae671271a41fb516c9ddcb8bcac50afa80
bryanmelendez <bryanmelendez2323@gmail.com>
2025-10-31 13:01:27 -0700

    - Logger class implemented - execution logs writting to a file

---
+++ utils/logger_test.py
@@ -0,0 +1,10 @@
+# Testing the Logger Class
+from logger import Logger
+
+log = Logger()
+
+log.log("General log message")
+log.warning("Warning")
+log.error("Error")
+log.debug("Debug")
+log.info("Info")
