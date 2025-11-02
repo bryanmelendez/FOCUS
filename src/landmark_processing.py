33ab4edbf964be99a45904cb826ce0b1c8ced23b
Matthew Quach <matthewquach@Matthews-MacBook-Pro-2.local>
2025-11-02 15:45:40 -0800

    started landmarks processing class

--- src/landmark_processing.py
+++ src/landmark_processing.py
@@ -1,1 +1,34 @@
-# Make a class in here called LandmarkProcessor or something like that
+# todo: imports
+
+class LandmarkProcessor:
+    def __init__(self):
+        # idk
+        pass
+    
+    def compute_EAR(self, landmarks):
+        # todo
+        pass
+    
+    def compute_MAR(self, landmarks):
+        # todo
+        pass
+
+    def compute_head_pose(self, landmarks, frame_shape):
+        # todo
+        pass
+
+    def compute_gaze(self, landmarks):
+        # todo
+        pass
+
+    def process_frame(self, landmarks):
+        EAR = self.compute_EAR(landmarks)
+        MAR = self.compute_MAR(landmarks)
+        head_pose = self.compute_head_pose(landmarks)
+        gaze = self.compute_gaze(landmarks)
+
+        results = {
+            "EAR": EAR, "MAR": MAR, "head_pose": head_pose, "gaze": gaze
+        }
+        return results
+
