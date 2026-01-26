0d032330781a586b539c4988f6f6a36e8b3f9279
mkquach <matthewkquach@gmail.com>
2026-01-26 13:25:23 -0800

    merged branches

--- src/main.py
+++ src/main.py
@@ -1,16 +1,6 @@
 from PySide6.QtWidgets import QApplication
 from gui.main_window import MainWindow
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.stderr = open(os.devnull, "w")
-from face_detection import FaceDetector
-from landmark_processing import LandmarkProcessor
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2
-from time import time, sleep
-from datetime import datetime
-from utils.logger import Logger
+import sys
 
 app = QApplication(sys.argv)
 
