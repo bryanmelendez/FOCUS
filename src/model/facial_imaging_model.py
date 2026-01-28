9f5aef8712cce8717273de548a0943ce8d6d7bb8
bryanmelendez <bryanmelendez2323@gmail.com>
2026-01-28 13:05:46 -0800

    merge done

---
+++ src/model/facial_imaging_model.py
@@ -0,0 +1,12 @@
+import cv2
+
+from utils.logger import Logger
+from face_detection import FaceDetector
+from landmark_processing import LandmarkProcessor
+
+class FacialImagingModel():
+    def __init__(self) -> None:
+        self.face_detector = FaceDetector()
+        self.landmark_processor = LandmarkProcessor()
+        self.logger = Logger()
+        self.cap: cv2.VideoCapture | None = None
